a6c09a3fa9b1210abbde5cc38368a8c43de446cd
johnnydev0 <joaoppessoa718@gmail.com>
2026-01-06 08:26:36 -0500

    Initial Commit

---
+++ alert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alert
+//
+//  Created by user289963 on 1/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alert/alertApp.swift
@@ -0,0 +1,17 @@
+//
+//  alertApp.swift
+//  alert
+//
+//  Created by user289963 on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct alertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
